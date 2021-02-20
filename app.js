9d7784f70b42ea38004bca5f9566572522333778
Kurt Guardia <kurtguardia@gmail.com>
2021-02-20 11:28:23 +0100

    implemented links and rendering pages

--- app.js
+++ app.js
@@ -3,3 +3,23 @@ const express = require('express');
 const app = express();
 
 app.listen(3000);
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Home' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'New Blog' });
+});
+
+app.use((req, res) => {
+  res.render('404', { title: 'Not Found' });
+});
