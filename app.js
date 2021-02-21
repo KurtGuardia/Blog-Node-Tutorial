e13a134278062ae04fdd878cfee0534f15f5f895
Kurt Guardia <kurtguardia@gmail.com>
2021-02-21 11:59:25 +0100

    connected to MongoDB using mongoose

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
+// express app
 const app = express();
 
-app.listen(3000);
+// connect to mongodb & listen for requests
+const dbURI =
+  'mongodb+srv://test:Test123@netninjatutotial.xkdfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => app.listen(3000))
+  .catch((err) => console.log(err));
 
+// register view engine
 app.set('view engine', 'ejs');
 
+// middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
+// routes
 app.get('/', (req, res) => {
   res.render('index', { title: 'Home' });
 });
@@ -20,6 +34,9 @@ app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'New Blog' });
 });
 
+// blog routes
+
+// 404 page
 app.use((req, res) => {
   res.render('404', { title: 'Not Found' });
 });
